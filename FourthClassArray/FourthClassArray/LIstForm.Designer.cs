45bae1fd8992ede4a58e049ea94eb92ea94fb5f1
hridoyict01 <mhfahad93@gmail.com>
2019-05-26 13:17:31 +0600

    Add listfrom to the winapp

--- FourthClassArray/FourthClassArray/LIstForm.Designer.cs
+++ FourthClassArray/FourthClassArray/LIstForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FourthClassArray
 {
-    partial class Form1
+    partial class LIstForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,69 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.InputTextBox = new System.Windows.Forms.TextBox();
             this.addButton = new System.Windows.Forms.Button();
+            this.inutTextBox = new System.Windows.Forms.TextBox();
             this.showRichTextBox = new System.Windows.Forms.RichTextBox();
             this.showButton = new System.Windows.Forms.Button();
-            this.reverseButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // InputTextBox
-            // 
-            this.InputTextBox.Location = new System.Drawing.Point(31, 64);
-            this.InputTextBox.Name = "InputTextBox";
-            this.InputTextBox.Size = new System.Drawing.Size(100, 20);
-            this.InputTextBox.TabIndex = 0;
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(138, 60);
+            this.addButton.Location = new System.Drawing.Point(166, 45);
             this.addButton.Name = "addButton";
             this.addButton.Size = new System.Drawing.Size(75, 23);
-            this.addButton.TabIndex = 1;
+            this.addButton.TabIndex = 0;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
             this.addButton.Click += new System.EventHandler(this.addButton_Click);
             // 
+            // inutTextBox
+            // 
+            this.inutTextBox.Location = new System.Drawing.Point(22, 48);
+            this.inutTextBox.Name = "inutTextBox";
+            this.inutTextBox.Size = new System.Drawing.Size(127, 20);
+            this.inutTextBox.TabIndex = 1;
+            // 
             // showRichTextBox
             // 
-            this.showRichTextBox.Location = new System.Drawing.Point(31, 90);
+            this.showRichTextBox.Location = new System.Drawing.Point(22, 85);
             this.showRichTextBox.Name = "showRichTextBox";
-            this.showRichTextBox.Size = new System.Drawing.Size(182, 324);
+            this.showRichTextBox.Size = new System.Drawing.Size(219, 278);
             this.showRichTextBox.TabIndex = 2;
             this.showRichTextBox.Text = "";
             // 
             // showButton
             // 
-            this.showButton.Location = new System.Drawing.Point(220, 100);
+            this.showButton.Location = new System.Drawing.Point(247, 94);
             this.showButton.Name = "showButton";
             this.showButton.Size = new System.Drawing.Size(75, 23);
             this.showButton.TabIndex = 3;
             this.showButton.Text = "Show";
             this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
             // 
-            // reverseButton
-            // 
-            this.reverseButton.Location = new System.Drawing.Point(219, 140);
-            this.reverseButton.Name = "reverseButton";
-            this.reverseButton.Size = new System.Drawing.Size(75, 23);
-            this.reverseButton.TabIndex = 4;
-            this.reverseButton.Text = "Reverse";
-            this.reverseButton.UseVisualStyleBackColor = true;
-            this.reverseButton.Click += new System.EventHandler(this.reverseButton_Click);
-            // 
-            // Form1
+            // LIstForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.reverseButton);
             this.Controls.Add(this.showButton);
             this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.inutTextBox);
             this.Controls.Add(this.addButton);
-            this.Controls.Add(this.InputTextBox);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "LIstForm";
+            this.Text = "LIstForm";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,11 +87,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox InputTextBox;
         private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox inutTextBox;
         private System.Windows.Forms.RichTextBox showRichTextBox;
         private System.Windows.Forms.Button showButton;
-        private System.Windows.Forms.Button reverseButton;
     }
 }
-
---
+++ FourthClassArray/FourthClassArray/LIstForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FourthClassArray
+{
+    public partial class LIstForm : Form
+    {
+        List<int> numbers = new List<int>();
+        public LIstForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            int input = Convert.ToInt32(inutTextBox.Text);
+           
+            numbers.Add(input);
+//            numbers.Add(1);
+//            numbers.Add(10);
+//            numbers.Add(20);
+//            numbers.Add(300);
+
+            string message = "Output \n";
+            foreach (int number in numbers)
+            {
+                message += number + "\n";
+            }
+
+            showRichTextBox.Text = message;
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            string message = "Output using show button: \n";
+            foreach (int number in numbers)
+            {
+                message += number + "\n";
+            }
+            showRichTextBox.Text = message;
+
+        }
+    }
+}
