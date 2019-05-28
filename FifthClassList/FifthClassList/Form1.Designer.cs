a2f3d312e03eed5f993b6f01f70b9b6422da084e
hridoyict01 <mhfahad93@gmail.com>
2019-05-28 13:06:25 +0600

    List item validation

--- FifthClassList/FifthClassList/Form1.Designer.cs
+++ FifthClassList/FifthClassList/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FifthClassList
 {
-    partial class Form1
+    partial class HomeForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(74, 60);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userTextBox.TabIndex = 0;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(74, 86);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(74, 112);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 2;
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(31, 151);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(292, 265);
+            this.showRichTextBox.TabIndex = 3;
+            this.showRichTextBox.Text = "";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(248, 122);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(609, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "User";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Age";
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(248, 66);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(0, 13);
+            this.userLabel.TabIndex = 9;
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Name = "HomeForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label userLabel;
     }
 }
 
--- FifthClassList/FifthClassList/Form1.cs
+++ FifthClassList/FifthClassList/Form1.cs
@@ -4,17 +4,99 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FifthClassList
 {
-    public partial class Form1 : Form
+    public partial class HomeForm : Form
     {
-        public Form1()
+
+        List<string> users = new List<string>();
+        List<string> names = new List<string>();
+        List<int> ages = new List<int>();
+        public HomeForm()
         {
             InitializeComponent();
+        }
+        
+        private void addButton_Click(object sender, EventArgs e)
+        {
+           
+            string user = userTextBox.Text;
+            string name;
+            string ageText = ageTextBox.Text;
+            int age;
+            bool ageConvert;
+            ageConvert = int.TryParse(ageText, out age);
+            userLabel.Text = "";
+
+            if (!ageConvert)
+            {
+                MessageBox.Show("Age must be integer");
+                return;
+            }
+ 
+            if (String.IsNullOrEmpty(user))
+            {
+                userLabel.Text = "User can't be empty";
+                return;
+            }
+
+            if (String.IsNullOrEmpty(nameTextBox.Text))
+            {
+                MessageBox.Show("User name cant not be empty");
+                return;
+            }
+
+            if (UserExist(user))
+            {
+                userLabel.Text = "User found,please change the user number";
+                return;
+            }
+
+            
+            name = nameTextBox.Text;
+            
+            
+            users.Add(user);
+            names.Add(name);
+            ages.Add(age);
+            string message;
+            message = Display();
+            showRichTextBox.Text = message;
+        }
+
+
+        public string Display()
+        {
+            string output;
+            output = "\t User \t Name \t Age \n";
+            foreach (string user in users)
+            {
+                int index = 0;
+                output +="\t"+ user+"\t" + names[index] + "\t" + ages[index]+"\n";
+                index++;
+            }
+            return output;
+        }
+
+        public bool UserExist(string user)
+        {
+            bool exists = false;
+            foreach (string userList in users)
+            {
+                if (userList == user)
+                {
+                    exists = true;
+                }
+                
+            }
+
+            return exists;
         }
     }
 }
